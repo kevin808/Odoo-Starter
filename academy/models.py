4545fe6f5cea77afb68f925a9a05512023fc3a60
kevin.lee <kevin.lee@elico-corp.com>
2015-05-15 16:52:19 +0800

    [IMP] LOGICAL

--- academy/models.py
+++ academy/models.py
@@ -47,7 +47,7 @@ class Session(models.Model):
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
-    seats = fields.Integer(string="Number of seats")
+    seats = fields.Integer(string="Number of seats",required=True)
     active = fields.Boolean(default=True)
     color = fields.Integer()
     instructor_id = fields.Many2one('res.partner', string="Instructor",
@@ -92,22 +92,23 @@ class Session(models.Model):
     		self.taken_seats = 100.0 * len(self.attendee_ids)/self.seats
 
 
-    @api.onchange('seats', 'attendee_ids')
-    def _verify_valid_seats(self):
-        if self.seats < 0:
-            return {
-                'warning': {
-                    'title': _("Incorrect 'seats' value"),
-                    'message': _("The number of available seats may not be negative"),
-                },
-            }
-        if self.seats < len(self.attendee_ids):
-            return {
-                'warning': {
-                    'title': _("Too many attendees"),
-                    'message': _("Increase seats or remove excess attendees"),
-                },
-            }
+    # @api.onchange('seats', 'attendee_ids')
+    # def _verify_valid_seats(self):
+    #     if self.seats < 0:
+    #         return {
+    #             'warning': {
+    #                 'title': _("Incorrect 'seats' value"),
+    #                 'message': _("The number of available seats may not be negative"),
+    #             },
+    #         }
+    #     if self.seats < len(self.attendee_ids):
+    #         return {
+    #             'warning': {
+    #                 'title': _("Too many attendees"),
+    #                 'message': _("Increase seats or remove excess attendees"),
+    #             },
+    #         }
+
 
     @api.one
     @api.depends('start_date', 'duration')
@@ -153,3 +154,10 @@ class Session(models.Model):
         if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
 
+    @api.one
+    @api.constrains('seats', 'attendee_ids')
+    def _check_seats_and_attendees(self):
+        if self.seats < 0:
+            raise exceptions.ValidationError(_("The number of available seats may not be negative"))                    
+        if self.seats < len(self.attendee_ids):
+            raise exceptions.ValidationError(_("Increase seats or remove excess attendees"))
